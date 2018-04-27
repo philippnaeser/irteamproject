b6352b71d615f4ab1b4b0a05c02c6a42f411b482
Larissa Heißler <heisslerlarissa@gmail.com>
2018-04-27 16:08:41 +0200

    Creating Point-wise approach template and add basic classifier

---
+++ pair_wise_approach/pair_wise_approach.py
@@ -0,0 +1,6 @@
+##Goal of pairwise approach is to find a exact relevance ranking of the documents regarding a specific query
+
+
+##create a full index od our document data because we want to compare each document with every other document
+#Important: we need to do this for every query, such that we have a 1:(n:n) matching
+#(1) c
