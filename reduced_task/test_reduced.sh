abdcdbcca6623f856bdf59a2758eaf238251f3b5
Philipp Naeser <philippnaeser@web.de>
2018-05-17 12:29:30 +0200

    Beispiel Model
    Mit Ranklib erstelltes Model und Evaluation auf das Testset.

--- reduced_task/test_reduced.sh
+++ reduced_task/test_reduced.sh
@@ -1,1 +1,1 @@
-java -jar RankLib-2.1-patched.jar -load rank_lib.model -test test.csv -metric2T MAP -gmax 2 -idv rank_lib_model.eval.txt
+java -jar RankLib-2.1-patched.jar -load rank_lib.mart.model -test test.csv -metric2T NDCG@10 -gmax 2 -idv rank_lib_ndcg.eval.txt
--- reduced_task/train_reduced.sh
+++ reduced_task/train_reduced.sh
@@ -1,1 +1,1 @@
-java -Xmx6g -jar RankLib-2.1-patched.jar -train train.csv -ranker 0 -metric2t MAP -gmax 2 -save rank_lib.model
+java -Xmx6g -jar RankLib-2.1-patched.jar -train train.csv -ranker 0 -metric2t NDCG@10 -gmax 2 -save rank_lib.mart.model
