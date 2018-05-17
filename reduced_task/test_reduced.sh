e10c39d0448a0d2d919c14168f62bae3947f4a6b
Philipp Naeser <philippnaeser@web.de>
2018-05-17 08:41:17 +0200

    Working sh scripts
    As commented in slack

--- reduced_task/test_reduced.sh
+++ reduced_task/test_reduced.sh
@@ -1,1 +1,1 @@
-java -jar RankLib-2.1-patched.jar -load rank_lib.model -test test.csv -metric2T MAP -gmax 2
+java -jar RankLib-2.1-patched.jar -load rank_lib.model -test test.csv -metric2T MAP -gmax 2 -idv rank_lib_model.eval.txt
