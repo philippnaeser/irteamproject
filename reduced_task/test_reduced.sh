29e6417cbeb98e15958f3c3ad9035e478c67e7c8
Philipp <philippnaeser@web.de>
2018-05-17 19:20:34 +0200

    Experimeniteren

--- reduced_task/test_reduced.sh
+++ reduced_task/test_reduced.sh
@@ -1,1 +1,1 @@
-java -jar RankLib-2.1-patched.jar -load rank_lib.mart.model -test test.csv -metric2T NDCG@10 -gmax 2 -idv rank_lib_ndcg.eval.txt
+java -jar RankLib-2.1-patched.jar -load rank_lib.mart.model -test test.csv -metric2T NDCG -gmax 2 -norm zscore -idv rank_lib_ndcg.eval.txt
