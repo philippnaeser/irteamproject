f00535324368def537196ed334747672de50b0b8
Philipp <philippnaeser@web.de>
2018-05-17 18:54:46 +0200

    Mit dev als validation set
    Allerdings nur minimale Verbesserung des NDCG@10 werts

--- reduced_task/train_reduced.sh
+++ reduced_task/train_reduced.sh
@@ -1,1 +1,1 @@
-java -Xmx6g -jar RankLib-2.1-patched.jar -train train.csv -ranker 0 -metric2t NDCG@10 -gmax 2 -save rank_lib.mart.model
+java -Xmx6g -jar RankLib-2.1-patched.jar -train train.csv -ranker 0 -metric2t NDCG@10 -gmax 2 -validate dev.csv -save rank_lib.mart.model
