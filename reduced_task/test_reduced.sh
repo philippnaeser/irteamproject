9329966f00cb55b412a8c51e4731d3ca45d2b4eb
philippnaeser <philippnaeser@web.de>
2018-05-18 19:05:44 +0200

    Mit Scores
    Commit wie besprochen, scores.zip enthält dataframes als pkl files,
    generiert mit 3.2.1 relevance. umbenennung von putting it all together
    zu feature generation

--- reduced_task/test_reduced.sh
+++ reduced_task/test_reduced.sh
@@ -1,1 +1,1 @@
-java -jar RankLib-2.1-patched.jar -load rank_lib.mart.model -test test.csv -metric2T NDCG -gmax 2 -norm zscore -idv rank_lib_ndcg.eval.txt
+java -jar RankLib-2.1-patched.jar -load rank_lib.3.2.1.ndcg.model -test test.csv -metric2T NDCG -gmax 3 -norm zscore -idv rank_lib.3.2.1_ndcg.eval.txt
--- reduced_task/train_reduced.sh
+++ reduced_task/train_reduced.sh
@@ -1,1 +1,1 @@
-java -Xmx6g -jar RankLib-2.1-patched.jar -train train.csv -ranker 0 -metric2t NDCG -gmax 2 -validate dev.csv -norm zscore -save rank_lib.ndcg.model
+java -Xmx6g -jar RankLib-2.1-patched.jar -train train.csv -ranker 0 -metric2t NDCG -gmax 3 -validate dev.csv -norm zscore -save rank_lib.3.2.1.ndcg.model
