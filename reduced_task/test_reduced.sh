f5fdb7fff81361fe3f076fd5a00d9c2ac532ebf4
philippnaeser <philippnaeser@web.de>
2018-05-20 00:33:36 +0200

    Freature Generation mit cosine
    Neue Datafiles und RankLib models, leicht umgepackt, um unter 100MB für
    github zu bleiben. Vorbereitende Schritte für Embeddings in feature
    generation

--- reduced_task/test_reduced.sh
+++ reduced_task/test_reduced.sh
@@ -1,1 +1,1 @@
-java -jar RankLib-2.1-patched.jar -load rank_lib.3.2.1.ndcg.model -test test.csv -metric2T NDCG -gmax 3 -idv rank_lib.3.2.1_ndcg.eval.txt
+java -jar RankLib-2.1-patched.jar -load rank_lib.321.ndcg.model -test test.csv -metric2T NDCG -gmax 3 -idv rank_lib.321.ndcg.eval.txt
--- reduced_task/train_reduced.sh
+++ reduced_task/train_reduced.sh
@@ -1,1 +1,1 @@
-java -Xmx6g -jar RankLib-2.1-patched.jar -train train.csv -ranker 0 -metric2t NDCG -gmax 3 -validate dev.csv -save rank_lib.3.2.1.ndcg.model
+java -Xmx6g -jar RankLib-2.1-patched.jar -train train.csv -ranker 0 -metric2t NDCG -gmax 3 -validate dev.csv -save rank_lib.321.ndcg.model
