476c0654ee344fdb14c89c7c185d5f14d7f80b96
Philipp <philippnaeser@web.de>
2018-05-24 16:39:18 +0200

    Changed relevance to 210
    And computed new query statistics accordingly

--- 3_ranklib_framework/pointwise_eval.sh
+++ 3_ranklib_framework/pointwise_eval.sh
@@ -1,1 +1,3 @@
-java -jar RankLib-2.1-patched.jar -load pointwise.ndcg1000.model -test test.csv -metric2T NDCG -gmax 3 -idv pointwise.ndcg.eval.txt
+java -jar RankLib-2.1-patched.jar -load pointwise.ndcg1000.model -test test.csv -metric2T NDCG@1000 -gmax 2 -idv pointwise.ndcg.eval.txt
+
+java -jar RankLib-2.1-patched.jar -load pointwise.ndcg1000.model -test test.csv -metric2T NDCG@10 -gmax 2s -idv pointwise.ndcg.eval.txt
