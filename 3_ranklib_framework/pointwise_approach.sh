23d582581ab9ebe4c02e0931aed18bf4ac8d28e5
philippnaeser <philippnaeser@web.de>
2018-05-25 20:39:13 +0200

    Computed Baselines
    For each Model

--- 3_ranklib_framework/pointwise_approach.sh
+++ 3_ranklib_framework/pointwise_approach.sh
@@ -1,4 +1,4 @@
-java -Xmx6g -jar RankLib-2.1-patched.jar -train train.csv -ranker 0 -metric2t NDCG@1000 -gmax 2 -validate dev.csv -save pointwise.ndcg1000.model
+java -Xmx6g -jar RankLib-2.1-patched.jar -train train.csv -ranker 0 -metric2t NDCG@10 -gmax 2 -validate dev.csv -save pointwise.ndcg10.model
 
-java -jar RankLib-2.1-patched.jar -load pointwise.ndcg1000.model -test test.csv -metric2T NDCG@1000 -gmax 2 -idv pointwise.ndcg1000.eval.txt
+java -jar RankLib-2.1-patched.jar -load pointwise.ndcg10.model -test test.csv -metric2T NDCG@10 -gmax 2 -idv pointwise.ndcg10.eval.txt
 
