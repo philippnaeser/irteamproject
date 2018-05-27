263f3ab7ce69a9bfd5ba44eaf9b60a35fc130f51
Philipp <hello@jamscikov.de>
2018-05-27 13:15:34 +0200

    excluding +/- from vocabulary > why? >> because high tfidf values in many docs

--- 3_ranklib_framework/pointwise_eval.sh
+++
@@ -1,3 +0,0 @@
-java -jar RankLib-2.1-patched.jar -load pointwise.ndcg1000.model -test test.csv -metric2T NDCG@1000 -gmax 2 -idv pointwise.ndcg.eval.txt
-
-java -jar RankLib-2.1-patched.jar -load pointwise.ndcg1000.model -test test.csv -metric2T NDCG@10 -gmax 2s -idv pointwise.ndcg.eval.txt
